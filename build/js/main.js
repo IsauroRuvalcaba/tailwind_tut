f6055db1da71e9d2fe955e00cfe3791cb21368b4
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-04-03 12:26:05 -0700

    added main.js javascript file that has a function to toggle between the webpage and the menu that is full. Added a link to this in the html file with scrip/src.  In the config file, added the location of this file to the content part so that tailwind knows to add new classes when this is used.

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    // this inserts or remove the class if not in there or is in there. Could also do this in a different type of function. More than one way to do this
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
