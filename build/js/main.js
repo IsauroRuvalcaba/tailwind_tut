c2b6cd38ee64796ffd9b0dd3299c85ae479378ce
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-04-03 17:45:54 -0700

    learned about Arbitrary Values in tailwind and how extract values with @apply. Created hamburger animation using the extract classes with apply in input.css by creating class .toggle-button. This class has animations and it is applied using the function in main.js.

--- build/js/main.js
+++ build/js/main.js
@@ -6,6 +6,9 @@ const initApp = () => {
     // this inserts or remove the class if not in there or is in there. Could also do this in a different type of function. More than one way to do this
     mobileMenu.classList.toggle("hidden");
     mobileMenu.classList.toggle("flex");
+
+    //this below, when mobile size, it turns hamburger to X when clicked. and reverse.
+    hamburgerBtn.classList.toggle("toggle-btn");
   };
 
   hamburgerBtn.addEventListener("click", toggleMenu);
